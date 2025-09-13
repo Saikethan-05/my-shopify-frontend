ad88e666e33208dfb25c1657ef40afdb9daa641c
Saikethan-05 <nskethan05@gmail.com>
2025-09-14 00:33:42 +0530

    App Error Fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./app";
+import App from "./App";
 import "./App.css"; // global styles
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
+
+console.log(`backend Url: ${process.env.REACT_APP_BACKEND_URL}`)
